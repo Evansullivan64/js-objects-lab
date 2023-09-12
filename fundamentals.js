a46c09e25bd1e18ca0cc27b088ce70e24ec0e2c6
Evansullivan64 <20093137@mail.wit.ie>
2023-09-12 14:51:49 +0100

    Loop constructs.

--- fundamentals.js
+++ fundamentals.js
@@ -23,6 +23,14 @@ const aCar = {
     exterior: "red",
     interior: { texture: "leather", shade: "cream" }
   }
+
+  for (let i = 0 ; i < aCar.features.length ; i += 1) {
+    console.log(aCar.features[i]) ;
+}
+
+for (let i = 0 ; i < aCar.previous_owners.length ; i += 1) {
+    console.log(aCar.previous_owners[i].name);
+}
   
   //console.log(aCar.owner);
   console.log(aCar.owner + ' drives a ' + aCar.type.make)
