a3f7687355254df830092debfd1dbcef47f8fa17
Evansullivan64 <20093137@mail.wit.ie>
2023-09-12 14:49:25 +0100

    Arrays.

--- fundamentals.js
+++ fundamentals.js
@@ -5,9 +5,12 @@ const aCar = {
         model:"corolla",
         cc:"1.8"
 
-    }
-    
-   ,
+    },
+    previous_owners: [
+        { name: "Pat Smith", address: "1 Main Street" },
+        { name: "Sheila Dwyer", address: "2 High Street" }
+      ],
+   
     registration : {
         year: "201",
         countycode: "WD",
@@ -17,11 +20,15 @@ const aCar = {
 
   aCar.milage = 10000
   aCar.color = {
-exterior:"red",
-interiorFabric: "leather",
-shade: "cream"
+    exterior: "red",
+    interior: { texture: "leather", shade: "cream" }
   }
   
   //console.log(aCar.owner);
   console.log(aCar.owner + ' drives a ' + aCar.type.make)
   console.log("reg = "+aCar.registration.year + "-"+aCar.registration.countycode+"-"+aCar.registration.number)
+  console.log("It is a " +      aCar.color.exterior + " car, " + aCar.milage +" milage, with " +aCar.color.interior.texture +
+      " interior."
+  );
+
+  console.log('First owner : ' + aCar.previous_owners[0] )
